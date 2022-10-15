92c4fa95aee6af9ebe85588dfceddc56123734a5
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-10-16 00:52:36 +0700

    restored old main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
-const display = document.querySelector("#main-display");
+const display = document.querySelector("#main-display")
 
-const con = display.getContext("2d");
+const con = display.getContext("2d")
 
-display.width = 800;
-display.height = 600;
+display.width = 800 * 0.8
+display.height = 600 * 0.8
+
+con.fillStyle = DarkSlateGray;
+con.fillRect(0, 0, display.width, display.height)
+
+class Sprite {
+    constructor(position, appearance, role) {
+        this.pos = position;
+        this.look = appearance
+        this.role = role;
+    }
+
+    draw() {
+        con.fillStyle = this.look;
+        con.fillRect(this.pos.x, this.pos.y, 60, 40);
+    }
+}
+
+
+
+const player = new Sprite({
+    x: 50,
+    y: 60
+}, "#F69EC0")
+
+player.draw();
+
+console.log(player);
