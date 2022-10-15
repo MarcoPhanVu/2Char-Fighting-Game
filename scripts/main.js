f88ba277f88ad846bd7c7788eb2d0ec79bdd0106
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-10-16 00:53:00 +0700

    fill color list

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,28 @@ const con = display.getContext("2d")
 display.width = 800 * 0.8
 display.height = 600 * 0.8
 
+
+// COLORS START
+
+GlossyGrape = "#A799B7";
+GlassyGrape = "#9888A5";
+OldLavender = "#776472";
+FeldGrau = "#445552";
+DarkSlateGray = "#294D4A";
+DimSkyBlue = "#83B5D1";
+Rythm = "#726E97";
+ParadisePink = "#EE4266";
+SunGlow = "#FFD23F";
+CaribeanGreen = "#3BCEAC";
+GoGreen = "#0EAD69";
+DavysGrey = "#595959";
+Xanadu = "#808F85";
+SunRay = "#EDB458"
+Celadon ="#BAD4AA";
+
+// COLORS END
+
+
 con.fillStyle = DarkSlateGray;
 con.fillRect(0, 0, display.width, display.height)
 
