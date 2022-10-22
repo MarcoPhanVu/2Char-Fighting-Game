f20791bbe57f2c0d6734ed2ae4daa02e2f5abc65
KiteryuKatori <88922348+KiteryuKatori@users.noreply.github.com>
2022-10-23 02:04:47 +0700

    re-arrange the codes, add movement,sprites.js

---
+++ scripts/movement.js
@@ -0,0 +1,153 @@
+let gravity = 1.2;
+
+function keepInside(obj) {
+    // if (obj.pos.y <= 0) { //Top
+    //     obj.pos.y = 0;
+    // }
+
+    if (obj.pos.y + 80 >= display.height) { // Ground
+        obj.pos.y = display.height - 80;
+        obj.inAir = false;
+        obj.velocity.y = 0; // to prevent weird Y offsets
+    }
+
+
+    if (obj.pos.x + 40 >= display.width) { //R ight
+        obj.pos.x = display.width - 40;
+    }
+    
+    if (obj.pos.x <= 0) { // left
+        obj.pos.x = 0;
+    }
+}
+
+function checkCollision(gameState) {
+    
+}
+
+const movement = {
+    keyDPressed: false,
+    keyAPressed: false,
+    keyWPressed: false,
+    keySPressed: false,
+    keyRAPressed: false,
+    keyLAPressed: false,
+    keyUAPressed: false,
+    keyDAPressed: false
+}
+
+let speedX = 8;
+let speedY = 20;
+
+function animate() {
+    con.clearRect(0, 0, display.width, display.height);
+    con.fillStyle = FeldGrau;
+    con.fillRect(0, 0, display.width, display.height);
+
+    //Avoid using elseif because we need to use multiple keys at once
+    //Player Section
+        if (movement.keyDPressed == true) {
+            player.velocity.x = speedX;
+        }
+
+        if (movement.keyAPressed == true) {
+            player.velocity.x = -speedX;
+        }
+
+        if (movement.keyWPressed == true && player.inAir == false) {
+            player.jump();
+        }
+
+    // Enemy Section
+        if (movement.keyRAPressed == true) {
+            enemy1.velocity.x = speedX;
+            enemy2.velocity.x = speedX;
+        }
+
+        if (movement.keyLAPressed == true) {
+            enemy1.velocity.x = -speedX;
+            enemy2.velocity.x = -speedX;
+        }
+
+        if (movement.keyUAPressed == true && enemy1.inAir == false && enemy2.inAir == false) {
+            enemy1.jump();
+            enemy2.jump();
+        }
+
+
+    player.update();
+    enemy1.update();
+    enemy2.update();
+
+    player.velocity = {x: 0, y: player.velocity.y};
+    enemy1.velocity = {x: 0, y: enemy1.velocity.y};
+    enemy2.velocity = {x: 0, y: enemy2.velocity.y};
+}
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+
+        // Player
+        case "a":
+            movement.keyAPressed = true;
+            break;
+        case "d":
+            movement.keyDPressed = true;
+            break;
+        case "w":
+            movement.keyWPressed = true;
+            break;
+        case "s":
+            movement.keySPressed = true;
+            break;
+
+
+        // Enemy
+        case "ArrowLeft":
+            movement.keyLAPressed = true;
+            break;
+        case "ArrowRight":
+            movement.keyRAPressed = true;
+            break;
+        case "ArrowUp":
+            movement.keyUAPressed = true;
+            break;
+        case "ArrowDown":
+            movement.keyDAPressed = true;
+            break;
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+    switch (event.key) {
+
+        // Player
+        case "a":
+            movement.keyAPressed = false;
+            break;
+        case "d":
+            movement.keyDPressed = false;
+            break;
+        case "w":
+            movement.keyWPressed = false;
+            break;
+        case "s":
+            movement.keySPressed = false;
+            break;
+
+
+        // Enemy
+        case "ArrowLeft":
+            movement.keyLAPressed = false;
+            break;
+        case "ArrowRight":
+            movement.keyRAPressed = false;
+            break;
+        case "ArrowUp":
+            movement.keyUAPressed = false;
+            break;
+        case "ArrowDown":
+            movement.keyDAPressed = false;
+            break;
+    }
+});
---
+++ scripts/sprite.js
@@ -0,0 +1,62 @@
+class Sprite {
+    constructor(position, appearance, velocity = {x: 0, y: 0}) {
+        this.pos = position;
+        this.look = appearance;
+        this.velocity = velocity;
+// SHIT, I WAS MAKING CHANGES ON AN INCORRECT BRANCHHHHHHH!
+// I'll leave this for my future self to fix
+
+// Me - future self: fuck you, past me, still unable to fix this. You should've went to sleep earlier instead of making these changes on the WRONG BRANCH!!!
+
+        this.spriteWidth = 40;
+        this.spriteHeight = 80;
+
+        this.weapon = {
+            posX: this.pos.x + this.spriteWidth,
+            posY: this.pos.y + this.spriteHeight,
+            width: 60,
+            height: 20
+        }
+        
+        // States
+        this.inAir = true;
+    }
+
+
+    draw() {
+        // Sprite
+        con.fillStyle = this.look;
+        con.fillRect(this.pos.x, this.pos.y, this.spriteWidth, this.spriteHeight);
+
+        // Weapon
+        con.fillStyle = Celadon;
+        con.fillRect(this.weapon.posX, this.weapon.posY, this.weapon.width, this.weapon.height);
+    }
+
+
+    update() {
+        if (this.inAir) {
+            this.velocity.y += gravity;
+        }
+
+        this.pos.x += this.velocity.x;
+        this.pos.y += this.velocity.y;
+
+        this.weapon.posX = this.pos.x + this.spriteWidth;
+        this.weapon.posY = this.pos.y;
+        // console.log(`Player posY = ${player.pos.y}`);
+        // console.log(`${this.name}: ${this.pos.y}`);
+        // console.log(`And loc: ${this.weapon.posY}:`)
+
+        keepInside(this);
+
+        this.draw();
+    }
+
+
+    jump() {
+        this.velocity.y = -speedY;
+        console.log(this.velocity.y);
+        this.inAir = true;
+    }
+}
